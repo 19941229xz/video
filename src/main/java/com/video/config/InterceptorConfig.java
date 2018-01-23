c9040e09c6660060124bda4c2811ca1a04c7928b
xiongzh@wesine.com <xiongzh@wesine.com>
2018-01-23 09:38:44 +0800

    bak

---
+++ src/main/java/com/video/config/InterceptorConfig.java
@@ -0,0 +1,28 @@
+package com.video.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.video.interceptor.TestInterceptor;
+
+@Configuration
+public class InterceptorConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// TODO Auto-generated method stub
+		
+		InterceptorRegistration ir=registry.addInterceptor(new TestInterceptor());
+		
+		ir.addPathPatterns("/**");//添加拦截路径
+		
+		//ir.excludePathPatterns("/");//添加不拦截的路径
+		
+		super.addInterceptors(registry);
+	}
+
+	
+	
+}
---
+++ src/main/java/com/video/interceptor/TestInterceptor.java
@@ -0,0 +1,37 @@
+package com.video.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class TestInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("拦截器：请求之前！");
+		
+		return false;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("拦截器：请求之中！无返回值！");
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("拦截器：请求之后！");
+	}
+	
+	
+	
+
+}
